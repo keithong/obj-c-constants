cb57e16c2123f64975104f84b2888e2395ca4f2b
Keith Samson <k.samson@klab.com>
2014-06-05 09:42:00 +0800

    Added currency identifier

--- Constants/main.m
+++ Constants/main.m
@@ -16,6 +16,10 @@ int main(int argc, const char * argv[])
         //In literal NSString, use \u for arbitrary unicode characters
         //In this example, we use \u03c0 for the symbol Pi
         NSLog(@"\u03c0 is %.2f", M_PI);
+
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:@"currency"];
+        NSLog(@"Money is %@", currency);
         
     }
     return 0;
